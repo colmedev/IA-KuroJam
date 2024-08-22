29cfe0ef9b535705d752dca3ac91ded032ea7731
colmedev <m.colmenarez.dev@gmail.com>
2024-08-22 19:00:08 -0400

    feat: :sparkles: create the navbar component

--- Frontend/src/components/Navbar/Navbar.tsx
+++ Frontend/src/components/Navbar/Navbar.tsx
@@ -2,24 +2,17 @@ import './Navbar.css'
 import { Button }  from '../Button/Button'
 export function Navbar() {
   
-  const links = [
-    {
-      link: "#",
-      text: "Home"
-    }
-  ]
 
   return (
     <>
     <ul className="Navbar">
-      {
-        links.map((link, index) => (
-          <li key={index} className="Navbar__item">
-            <a href={link.link}>{link.text}</a>
-          </li>
-        ))
-      }
-      <Button text="Get Started" link="/app"/>
+      <li className='Navbar__item'>
+      <Button text="Login" link="#"/>
+      </li>
+
+      <li className='Navbar__item'>
+      <Button text="Sign Up" link="#"/>
+      </li>
     </ul>
     </>
   )
