079046cfc23cf81e7c9b4de8496c319062e68d4e
colmedev <m.colmenarez.dev@gmail.com>
2024-08-22 19:02:33 -0400

    feat: :sparkles: create the index.ts into components
    index.ts impove the way to export the components

--- Frontend/src/components/index.ts
+++ Frontend/src/components/index.ts
@@ -1,3 +1,6 @@
 export { Navbar } from "./Navbar/Navbar";
 export { Button } from "./Button/Button";
 export { Input } from "./ChatComponents/Input";
+export { Card } from "./Card/Card"
+export { Footer } from "./Footer/Footer"
+export { Cta } from "./SectionCtA/CtA"
