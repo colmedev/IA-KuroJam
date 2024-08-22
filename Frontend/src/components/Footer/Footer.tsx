26374554f64188b9bd2d9a63f867caa788a3663c
colmedev <m.colmenarez.dev@gmail.com>
2024-08-22 19:01:14 -0400

    feat: :sparkles: create the footer component

---
+++ Frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+export const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>Made by Miguel Colmenarez & Luis Matos with ❤</p>
+      </footer>
+    </>
+  )
+}
