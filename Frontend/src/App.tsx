8c5a0c14fed0d8d4f477f68e3741c5a2e7d2b00d
colmedev <m.colmenarez.dev@gmail.com>
2024-08-22 19:03:46 -0400

    feat: :sparkles: add the components into the project

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,55 @@
-import { Navbar } from './components'
-
+import Layout from './Layout/Layout'
+import { Card, Cta, Footer } from './components'
 
 function App() {
 
   return (
     <>
-    <Navbar />
+    <div className='container'>
+    <Layout />
 
+    <div className="career__path">
+      <h2>Explore Different Career Paths</h2>
+      <p>Find the perfect career that aligns with your passions and strengths.</p>
+      <div className='career__cards'>
+        <Card 
+        icon='briefCase'
+        title='Business and Finance'
+        description='Explore careers in accounting, marketing, management, and more.'
+        />
+        <Card 
+        icon='Code'
+        title='Business and Finance'
+        description='Explore careers in accounting, marketing, management, and more.'
+        />
+        <Card 
+        icon='Heart'
+        title='Business and Finance'
+        description='Explore careers in accounting, marketing, management, and more.'
+        />
+        <Card 
+        icon='Palette'
+        title='Business and Finance'
+        description='Explore careers in accounting, marketing, management, and more.'
+        />
+        <Card 
+        icon='School'
+        title='Business and Finance'
+        description='Explore careers in accounting, marketing, management, and more.'
+        />
+        <Card 
+        icon='rrhh'
+        title='Business and Finance'
+        description='Explore careers in accounting, marketing, management, and more.'
+        />
+        
+      </div>
+    </div>
+
+    <Cta />
+
+    <Footer />
+    </div>
 
     </>
   )
