b1a14a6dc8f86bd4436e0dfa27aef55ca3fe0fde
colmedev <m.colmenarez.dev@gmail.com>
2024-08-28 16:58:17 -0400

    feat: :sparkles: add signin and login buttons into the navbar

--- Frontend/src/components/Navbar/Navbar.tsx
+++ Frontend/src/components/Navbar/Navbar.tsx
@@ -1,19 +1,13 @@
 import './Navbar.css'
-import { Button }  from '../Button/Button'
+import { Signin } from '../auth/Signin'
+import { Login } from '../auth/Login'
 export function Navbar() {
   
 
   return (
     <>
-    <ul className="Navbar">
-      <li className='Navbar__item'>
-      <Button text="Login" link="#"/>
-      </li>
-
-      <li className='Navbar__item'>
-      <Button text="Sign Up" link="#"/>
-      </li>
-    </ul>
+    <Login />
+    <Signin />
     </>
   )
 }
