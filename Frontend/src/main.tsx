f535ad3d7c19aecc8f7083a6db8bf055a8f15014
colmedev <m.colmenarez.dev@gmail.com>
2024-08-28 16:55:48 -0400

    feat: :sparkles: add clerk provider to sign in

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,15 +1,23 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { Chat } from './pages/Chat.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { Chat } from './pages/Chat.tsx'
+import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
+const PUBLISHABLE_KEY = import.meta.env.VITE_PUBLISHED_KEY;
 
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
+createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <Routes>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <Routes>
         <Route path="/" element={<App />} />
         <Route path='/app' element={<Chat />} />
-    </Routes>
+      </Routes>
+    </ClerkProvider>
   </BrowserRouter>,
 )
